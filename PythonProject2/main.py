15dacbb40cf6699c42cf4427a93809255da500f3
LeneaM <0421108@MY.SCCCD.EDU>
2026-01-29 07:41:40 -0800

    Add files via upload
    Chapter 2 code as I learned something new each section.

--- PythonProject2/main.py
+++ PythonProject2/main.py
@@ -1,17 +1,62 @@
-message = 'Dracula'
-n = 34
-pi = 3.14
+from wsgiref.types import StartResponse
 
-print(34, message, pi)
+a = 5+5
+b = 10-5
+c = 5*5
+d = 10/5
+e = 20**5
 
->>> type(message)
-<class 'str'>
->>> type(n)
-<class 'int'>
->>> type(pi)
-<class 'float'>
+print(a,b,c,d,e)
 
+#pemdas
 
+x = a + b
+y = e - c
+z = (b*b) + d
+print(x,y,z)
+
+day = 1
+month = 30*day
+year = 12*month + 5*day
+print(day,month,year)
+
+#Exercise 1
+
+5
+x = 5
+x = 1
+
+# modulus operator
+
+quotient = 7 // 3
+print(quotient)
+remainder = 7 % 3
+print(remainder)
+
+#string operations
+
+primero = 5
+segundo = 10            #yay I can use spanish
+print(primero+segundo)
+primero = '5'
+segundo = '10'
+print(primero + segundo)
+
+first = ' Holy '
+second = 'Moly '
+third = 'Batman!'           #kinda fun
+fourth = 3
+print((first + second + third) * fourth)
+
+#mneumonic names...
+
+name = 'Lenea'
+surname = 'McGuire'
+origin = 'California'
+birth = 1987
+year = 2026
+age = year - birth
+print(age)
 
 
 
